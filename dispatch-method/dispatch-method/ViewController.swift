4c624f46c01438e954bd044aaff8401daf1959c4
Ruslan Khanov <hanov@technokratos.com>
2021-09-20 18:45:33 +0300

    create init project

---
+++ dispatch-method/dispatch-method/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dispatch-method
+//
+//  Created by Ruslan Khanov on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
